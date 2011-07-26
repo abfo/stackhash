efe206c802c1d82d681b2a35ceae8e4add639722
Robert Ellison <rob@catfood.net>
2011-07-26 18:42:24 +0000

    Adding StackHash.EmailPlugin

---
+++ stackhash/Plugins/StackHash.EmailPlugin/StackHash.EmailPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System;
+
+[assembly: AssemblyTitle("StackHash Email Plugin")]
+[assembly: AssemblyDescription("Email Plugin for StackHash")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StackHash")]
+[assembly: AssemblyCopyright("Copyright © Cucku, Inc and the stackhash.codeplex.com contributors.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7a91417b-f954-4c87-b5c7-bc864cdec5ee")]
+
+[assembly: AssemblyVersion("1.30.18911.65")]
+[assembly: AssemblyFileVersion("1.30.18911.65")]
