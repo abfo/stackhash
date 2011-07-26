0cbf5bd1a885f4f82b2b237e2df531f6aeebfde3
RobEllison_cp <SND\RobEllison_cp>
2011-07-26 18:43:29 +0000

    Adding StackHash.FogBugzPlugin

---
+++ stackhash/Plugins/StackHash.FogBugzPlugin/StackHash.FogBugzPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System;
+
+[assembly: AssemblyTitle("StackHash FogBugz Plugin")]
+[assembly: AssemblyDescription("FogBugz Plugin for StackHash")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StackHash")]
+[assembly: AssemblyCopyright("Copyright © Cucku, Inc and the stackhash.codeplex.com contributors.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ec3dff5c-e15a-43ea-98d7-fff748048f65")]
+
+[assembly: AssemblyVersion("1.30.18911.65")]
+[assembly: AssemblyFileVersion("1.30.18911.65")]
