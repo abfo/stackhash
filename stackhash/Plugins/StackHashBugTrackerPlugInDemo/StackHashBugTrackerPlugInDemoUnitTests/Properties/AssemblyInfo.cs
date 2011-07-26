120e334d8bd9a70ad4cf0befc3b0c586d0dee5a9
Robert Ellison <rob@catfood.net>
2011-07-26 18:44:30 +0000

    Adding StackHashBugTrackerPlugInDemo

---
+++ stackhash/Plugins/StackHashBugTrackerPlugInDemo/StackHashBugTrackerPlugInDemoUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StackHashBugTrackerPlugInDemoUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cucku, Inc.")]
+[assembly: AssemblyProduct("StackHashBugTrackerPlugInDemoUnitTests")]
+[assembly: AssemblyCopyright("Copyright © Cucku, Inc. 2010-2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("409774e4-0ccd-450c-9252-1bf8198a6ff8")]
+
+[assembly: AssemblyVersion("1.30.18911.65")]
+[assembly: AssemblyFileVersion("1.30.18911.65")]
